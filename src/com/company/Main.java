35863151dbfac62b0b63936adc77322ddad3bf90
Lillian Begley <lillianbegley@Lillians-MacBook-Air.local>
2021-11-08 20:34:47 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Main {
+
+	public static void main(String[] args) {
+		{
+			System.out.println("hello world");
+
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		book book1 = new book();
+//		book book2 = new book();
+//		book1 author("Lillian");
+
+//		int[]list1 = {1, 2, 3, 4, 5, 6};
+//		System.out.println(list1[6]);
+
+
+
+
+
+
+
+
+
+
+
+
+
